5c7bedd5f3b2fdb721b431eacfc1e121cecb503f
선규 <106080105+sungyu1@users.noreply.github.com>
2023-01-09 23:37:06 +0900

    js: Home transparent

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 'use strict'
 
-// navbar 를 아래로 내리면 투명하게 만들기
+// navbar 를 아래로 내리면 navbar이 나오기
 const navbar=document.querySelector('#navbar');
 const navbarHeight=navbar.getBoundingClientRect().height;
 document.addEventListener('scroll',()=>{
@@ -27,6 +27,20 @@ const homeContact=document.querySelector('.home__contact');
 homeContact.addEventListener('click',()=>{
   scrollIntoView('#contact');
 })
+
+
+// 스크롤을 내리면 페이지가 투명하게 만들기
+const navbarscroll=document.querySelector('#navbar')
+
+const home=document.querySelector('.home__container');
+const homeHeight=home.getBoundingClientRect().height;
+document.addEventListener('scroll',()=>{
+  home.style.opacity=1-window.scrollY/homeHeight;
+
+});
+
+
+
 
 function scrollIntoView(selector){
   const scrolTo=document.querySelector(selector);
